33ad56f557e375249fa3c99fb384f8b236e220e6
Dijana Najdovska <najdovskadijana@gmail.com>
2017-04-26 23:32:37 +0200

    tasks fuctionality with the repository pattern

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
+
+Route::group(['prefix' => 'tasks'], function() {
+
+	Route::get('/{id?}', ['as' => 'task.index', 'uses' => 'TaskController@getAllTasks']);
+
+	Route::post('store', ['as' => 'task.store', 'uses' => 'TaskController@postStoreTask']);
+
+	Route::patch('/{id}/update', ['as' => 'task.update', 'uses' => 'TaskController@postUpdateTask']);
+
+	Route::delete('/{id}/delete', ['as' => 'task.delete', 'uses' => 'TaskController@postDeleteTask']);
+
+});
